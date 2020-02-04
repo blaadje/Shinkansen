19b5c47f7fb0fc662fa27648b3d8f5a93882381f
blaadje <blaadje@users.noreply.github.com>
2020-02-04 00:27:31 +0000

    deployed via Github Pages Deploy Action 🚀 for a7332d4e4e887167f3cddcc7b827ae4718116fac

---
+++ precache-manifest.abe684df8d277229cb907ac138c6fc36.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e60d074780869205b39cd6c0526aab0",
+    "url": "/Shinkansen/index.html"
+  },
+  {
+    "revision": "dfedfa0adb86e75c3a01",
+    "url": "/Shinkansen/static/js/2.2684e85e.chunk.js"
+  },
+  {
+    "revision": "813de327701bbcb04597",
+    "url": "/Shinkansen/static/js/main.fb8db337.chunk.js"
+  },
+  {
+    "revision": "99056babccfcbe1397d9",
+    "url": "/Shinkansen/static/js/runtime~main.27f56b84.js"
+  }
+]);
