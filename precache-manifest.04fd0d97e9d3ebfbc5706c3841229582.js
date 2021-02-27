db96e7851ee2885b12dd185bde3401ebaa7fa9d2
blaadje <acharlot91@gmail.com>
2021-02-27 16:11:47 +0000

    Deploying to gh-pages from  @ c2b99657398c384edecbcc543f3f1cd3b2bf1e1f 🚀

---
+++ precache-manifest.04fd0d97e9d3ebfbc5706c3841229582.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1926995029182ff76f8fb1ec4837b193",
+    "url": "/Shinkansen/index.html"
+  },
+  {
+    "revision": "0faed5f939a23703a87c",
+    "url": "/Shinkansen/static/js/2.1de5904d.chunk.js"
+  },
+  {
+    "revision": "b3e6df22abd030d7db51",
+    "url": "/Shinkansen/static/js/main.d2cde398.chunk.js"
+  },
+  {
+    "revision": "99056babccfcbe1397d9",
+    "url": "/Shinkansen/static/js/runtime~main.27f56b84.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Shinkansen/precache-manifest.461b49fb88a8897c549a80d4b4ea2082.js"
+  "/Shinkansen/precache-manifest.04fd0d97e9d3ebfbc5706c3841229582.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Shinkansen/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
